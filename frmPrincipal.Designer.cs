579c6cb76387398a47da7ce8072861abbf0a3faa
Yosu YCR <jacen.s@gmail.com>
2021-01-13 17:08:22 +0100

    finalizacion

---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace cbzCreator
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.btnExaminar = new System.Windows.Forms.Button();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Location = new System.Drawing.Point(13, 13);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(524, 20);
+            this.txtRuta.TabIndex = 0;
+            // 
+            // btnExaminar
+            // 
+            this.btnExaminar.Location = new System.Drawing.Point(543, 10);
+            this.btnExaminar.Name = "btnExaminar";
+            this.btnExaminar.Size = new System.Drawing.Size(29, 23);
+            this.btnExaminar.TabIndex = 1;
+            this.btnExaminar.Text = "...";
+            this.btnExaminar.UseVisualStyleBackColor = true;
+            this.btnExaminar.Click += new System.EventHandler(this.btnExaminar_Click);
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(578, 11);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(121, 23);
+            this.btnConvertir.TabIndex = 2;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(13, 40);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(686, 394);
+            this.lstLog.TabIndex = 3;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(707, 450);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.btnExaminar);
+            this.Controls.Add(this.txtRuta);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(723, 489);
+            this.MinimumSize = new System.Drawing.Size(723, 489);
+            this.Name = "frmPrincipal";
+            this.Text = "CBZ Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.Button btnExaminar;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox lstLog;
+    }
+}
+
---
+++ frmPrincipal.cs
@@ -0,0 +1,49 @@
+﻿using Rebex.IO.Compression;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cbzCreator
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExaminar_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtRuta.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo rootDir = new System.IO.DirectoryInfo(txtRuta.Text);
+            System.IO.DirectoryInfo[] subDirs = rootDir.GetDirectories();
+            foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+            {
+                // Resursive call for each subdirectory.
+                string archivoZip = txtRuta.Text + @"\" + dirInfo.ToString() + ".cbz";
+                ZipArchive archive = new ZipArchive(archivoZip);
+                ArchiveOperationResult result = archive.Add(
+                        txtRuta.Text + @"\" + dirInfo.ToString(),
+                        @"\",
+                        ArchiveTraversalMode.Recursive,
+                        ArchiveActionOnExistingFile.OverwriteAll
+                    );
+                lstLog.Items.Add(dirInfo.ToString() + ", "+result.FilesAffected.ToString()+" archivos añadidos ");
+                break;
+            }            
+        }
+    }
+}
